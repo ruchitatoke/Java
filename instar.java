752d4bbbbdfb3f8b481862e08abeca2546fb485e
ruchita toke <tokeruchita@gmail.com>
2023-04-26 23:26:55 +0530

    initial commit

---
+++ instar.java
@@ -0,0 +1,10 @@
+class instar {
+    public static void main(String[] args) {
+        for(int line=1;line<=6;line++){
+            for(int star=1;star<=6-line+1;star++){
+              System.out.print("*");  
+            }
+        System.out.println();
+    }
+  }
+}
