65ae63607f95d1cb2e699d8c90042493f648036d
ruchita toke <tokeruchita@gmail.com>
2023-05-10 07:50:33 +0530

    initial commit

---
+++ binary_oddeven.java
@@ -0,0 +1,13 @@
+public class binary_oddeven {
+    public static void main(String args[]){
+        int n=3;
+        int bitmask=1;
+        if((n&bitmask)==0){
+            System.out.println("even number");
+        }
+        else{
+            System.out.println("odd number");
+        }
+    }
+    
+}
