5b37c3cb3b5273fc01f5c185f3a69a6202e2efda
ruchita toke <tokeruchita@gmail.com>
2023-05-21 13:42:51 +0530

    initial commit

---
+++ fibonacci_recursion.java
@@ -0,0 +1,16 @@
+//Finding Fibonacci place using recursion
+
+public class fibonacci_recursion {
+    public static void main(String[] args) {
+        int ans=fibo(8);
+        System.out.println(ans);
+    }
+    static int fibo(int n){
+        if(n<2){
+            return n;
+            
+        }
+        return fibo(n-1)+fibo(n-2);
+        
+    }
+}
