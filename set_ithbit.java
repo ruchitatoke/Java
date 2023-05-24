a22692cf4aee9db4f483eed3df31a864f666f669
ruchita toke <tokeruchita@gmail.com>
2023-05-24 18:53:54 +0530

    initial commit

---
+++ set_ithbit.java
@@ -0,0 +1,9 @@
+public class set_ithbit {
+    public static void main(String args[]){
+        int n=10;
+        int i=2;
+        int bitmask=1<<i;
+        
+        System.out.println(n | bitmask);
+    }
+}
