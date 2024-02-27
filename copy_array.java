b7d7b6369f1eb65b85134087882da8a7c547ea98
ruchita toke <tokeruchita@gmail.com>
2024-02-27 20:18:30 +0530

    initial commit

---
+++ copy_array.java
@@ -0,0 +1,19 @@
+public class copy_array {
+    public static void main(String[] args) {
+        
+        int arr[] = new int[] {1,2,3,4,5};
+        int arr1[] = new int[arr.length];
+
+        for(int i=0; i<arr.length;i++){
+            arr1[i]=arr[i];
+           
+        }
+        
+        System.out.print("Elements of new array : ");
+        for(int i=0; i<arr1.length; i++){
+            System.out.print(arr1[i]+ " ");
+        }
+
+
+    }
+}
